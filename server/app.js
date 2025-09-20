6bc8f48c68b095ed4ee41b351608086259a414d5
raghuram <raghurammallimoggala@gmail.com>
2025-09-20 17:30:02 +0530

    first commitInitial commit - Customer Management App backend

--- server/app.js
+++ server/app.js
@@ -16,10 +16,10 @@ const initializeDB=async()=>{
             driver:sqlite3.Database
          
         })
-        const port=5000
-        app.listen(port,()=>{
-            console.log(`Server is running on port ${port}`)
-        })
+        const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`✅ Server running on port ${PORT}`);
+    });
     }catch(error){
         console.log(`DB Error:${error.message}`)
         process.exit(1)
