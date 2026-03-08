61b2f34f411961d3431694d3bfffcefbd6c5abc9
raghuram <raghurammallimoggala@gmail.com>
2026-03-08 08:41:26 +0530

    add sqlite in package.json

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const initializeDB=async()=>{
             driver:sqlite3.Database
          
         })
-        const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`✅ Server running on port ${PORT}`);
     });
