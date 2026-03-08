fc73727cfe1a04f44af2ab1dc058fe8082565857
raghuram <raghurammallimoggala@gmail.com>
2026-03-08 08:58:29 +0530

    add gitignore

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const initializeDB=async()=>{
         })
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
-      console.log(`✅ Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
     }catch(error){
         console.log(`DB Error:${error.message}`)
